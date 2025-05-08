d063a8a55197a4cce04320a01a5882140d714890
Raul Battistini <raulbattistini.3@gmail.com>
2025-05-08 14:16:33 -0300

    chore: adapting schemas to follow entities definition

--- be-express/src/modules/commentary/schema.ts
+++ be-express/src/modules/commentary/schema.ts
@@ -4,6 +4,8 @@ const CommentarySchema = z.object({
    id: z.number().int(),
    userId: z.string().uuid(),
    createdAt: z.date(),
+   title: z.string().max(50),
+   commentary: z.string().max(255),
 });
 
 export type COMMENTARY = z.infer<typeof CommentarySchema>;
--- be-express/src/modules/ingredient/schema.ts
+++ be-express/src/modules/ingredient/schema.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 const IngredientSchema = z.object({
    id: z.number().int(),
    recipeId: z.string().max(50),
+   title: z.string().max(50),
    ingredientId: z.string().max(255),
    quantity: z.number().positive(),
    note: z.string().max(255),
